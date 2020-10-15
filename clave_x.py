8604547336b1da5fba4bebbe55985a959b22d48b
javier8osorio <71994352+javier8osorio@users.noreply.github.com>
2020-10-14 22:06:50 -0600

    Add files via upload

---
+++ clave_x.py
@@ -0,0 +1,60 @@
+import math
+
+"""
+***************************************************************
+@@ ejercicio 1 @@
+un metodo python que haga la suma de 2 numeros
+2+4 = 6
+"""
+
+
+# start-->
+def suma(numero1, numero2):
+    resultado = numero1 + numero2
+
+    return resultado
+
+
+"""
+***************************************************************
+@@ ejercicio 2 @@
+un metodo python que haga la multiplicacion de 3 numeros
+2*4*5 = 40
+"""
+
+
+# start-->
+def multiplicacion(numero1, numero2, numero3):
+    resultado = numero1 * numero2 * numero3
+
+    return resultado
+
+
+"""
+***************************************************************
+@@ ejercicio 3 @@
+un metodo python que haga la suma de los numeros de la lista
+numerosLista = [2,5,4,6,9,12]
+"""
+
+
+# start-->
+def sumarLista():
+    listaNumeros = [2, 5, 4, 6, 9, 12]
+    resultado = sum(listaNumeros)
+
+    return resultado
+
+
+"""
+***************************************************************
+@@ ejercicio 4 @@
+colocar este proyecto en github
+colocar aca debajo la url
+enviar la url al correo balbino_a@hotmail.com
+"""
+
+
+# github url-->
+def getGithubUrl():
+    return ""
